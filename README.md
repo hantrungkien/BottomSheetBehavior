e8cc45e9ef5bbbf18a82f2fe325cca96b3f79f3d
KienHT <kienhantrung@gmail.com>
2018-09-05 19:42:35 +0700

    ver 1.0.0

---
+++ README.md
@@ -0,0 +1,50 @@
+# BottomSheetBehavior
+BottomSheetBehavior is an android library extracted from the Google I/O 2018 application source code.
+
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Platform](https://img.shields.io/badge/platform-android-green.svg)](http://developer.android.com/index.html)
+[![](https://jitpack.io/v/hantrungkien/BottomSheetBehavior.svg)](https://jitpack.io/#hantrungkien/BottomSheetBehavior)
+
+<a><img src="./image/screenshot.png" width="200"></a>
+
+### install:
+
+**via JitPack (to get current code)**
+
+project/build.gradle
+````gradle
+allprojects {
+    repositories {
+        maven { url "https://jitpack.io" }
+    }
+}
+````
+module/build.gradle
+````gradle
+implementation 'com.github.hantrungkien:BottomSheetBehavior:v1.0.0'
+````
+
+#### How to use please review in the demo app
+
+### Contribution
+
+If you've found an error, please file an issue.
+
+Patches and new samples are encouraged, and may be submitted by forking this project and submitting a pull request through GitHub.
+
+
+### LICENCE
+
+    Copyright 2018 Kien Han Trung
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ app/src/main/java/com/kienht/demobottomsheetbehavior/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.kienht.demobottomsheetbehavior
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
+import com.kienht.bottomsheetbehavior.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet)
+
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                val rotation = when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> 0f
+                    BottomSheetBehavior.STATE_COLLAPSED -> 180f
+                    BottomSheetBehavior.STATE_HIDDEN -> 180f
+                    else -> return
+                }
+
+                expand_icon.animate().rotationX(rotation).start()
+            }
+        })
+
+        clickable.setOnClickListener {
+            if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_COLLAPSED) {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+            } else {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+            }
+        }
+
+        description_scrollview
+                .setOnScrollChangeListener { v: NestedScrollView, _: Int, _: Int, _: Int, _: Int ->
+                    sheet_header_shadow.isActivated = v.canScrollVertically(-1)
+                }
+
+        marker_description.setText("OICSoft.com - Many start-ups in our portfolio had no technical staff, and we do know how to deal with it. Our team studies the specifics of your projects and can build the entire software development process for non-technical clients. Over the years, we've faced many problems and found numerous workarounds. That's how we polished our software development process, and now can confidently guarantee quality and timely results.")
+    }
+}
